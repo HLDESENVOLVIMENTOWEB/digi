32e257180e931a557a248a3e332be8cdd7450322
hyuri <hyuri.miranda.cortes@gmail.com>
2024-03-07 20:11:10 -0300

    feat: projeto finalizado

--- frontend/src/components/ProductCard/index.tsx
+++ frontend/src/components/ProductCard/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useCart } from '../../contexts/CartContext'; 
 import { Product } from '../../types/Product';
-import { ProductCard, ProductImage, ProductInfo, ProductName, ProductPrice, BuyButton } from '../Products/styles'; 
+import { ProductCard, ProductImage, ProductInfo, ProductName, ProductPrice, BuyButton, ButtonContainer, BuyButton2 } from '../Products/styles'; 
 
 interface ProductCardProps {
   product: Product;
@@ -13,21 +13,28 @@ const ProductCardComponet: React.FC<ProductCardProps> = ({ product }) => {
   const navigate = useNavigate();
 
   const handleBuy = () => {
-    console.log(product)
     addToCart(product, 1); 
     navigate('/cart'); 
+  };
+
+  const handleAddBuy = () => {
+    addToCart(product, 1); 
   };
 
   return (
     <ProductCard key={product.id}>
       <ProductImage src={product.image} alt={product.name} />
       <ProductInfo>
-        <ProductName>{product.name}</ProductName>
-        <p>{product.detail}</p>
-        {product.info && <p>Info: {product.info}</p>} 
-        {product.offer && <p>Oferta: {product.offer}</p>} 
-        <ProductPrice>${product.price}</ProductPrice>
-        <BuyButton onClick={handleBuy}>Comprar</BuyButton>
+          <ProductName>{product.name}</ProductName>
+          <p>{product.detail}</p>
+          {product.info && <p>Info: {product.info}</p>} 
+          {product.offer && <p>Oferta: {product.offer}</p>} 
+          <ProductPrice>${product.price}</ProductPrice>  
+        
+        <ButtonContainer>
+          <BuyButton onClick={handleBuy}>Comprar</BuyButton>
+          <BuyButton2 onClick={handleAddBuy}>Adicionar ao carrinho</BuyButton2>
+        </ButtonContainer>
       </ProductInfo>
     </ProductCard>
   );
